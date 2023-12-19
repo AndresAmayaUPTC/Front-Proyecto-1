743b733091db70b87461ec69b089d8cea0b93be4
Andres Amaya <andre.amaya03@uptc.edu.co>
2023-12-19 08:25:11 -0500

    Vistas de ubicaciones y alquileres

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ const path = require('path')
 route.get('/', (req, res)=>res.sendFile(path.join(__dirname,'./../views/index.html')))
 route.get('/rent', (req, res)=>res.sendFile(path.join(__dirname,'./../views/rent.html')))
 route.get('/add-location', (req, res)=>res.sendFile(path.join(__dirname,'./../views/location.html')))
+route.get('/view-rents', (req, res)=>res.sendFile(path.join(__dirname,'./../views/rentlist.html')))
+route.get('/view-locations', (req, res)=>res.sendFile(path.join(__dirname,'./../views/locationlist.html')))
 
 module.exports = route
